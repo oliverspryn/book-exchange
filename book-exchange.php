77980b4374d17872abf64ac2d4c738958c230c21
ffiadmin <admin@forwardfour.com>
2013-01-25 15:31:12 -0500

    This revision creates the initial basic structure of the Wordpress plugin, without any functionality.

---
+++ book-exchange.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Book Exchange
+Plugin URI: http://forwardfour.com/book-exchange
+Description: This is a plugin for the Grove City College Student Government Association's student textbook exchange.
+Version: 2.0
+Author: Oliver Spryn
+Author URI: http://forwardfour.com/
+License: MIT
+*/
+?>
