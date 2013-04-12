81779040ee70b1e7a8f2297e123891e280e58453
ffiadmin <admin@forwardfour.com>
2013-04-11 21:16:18 -0400

    global.php was added, as it should have been in the previous revision.

---
+++ global.php
@@ -0,0 +1,6 @@
+<?php
+$essentials->includeCSS("//delfinicdn.appspot.com/stylesheets/delfini.all.min.css");
+$essentials->includeCSS("//ajax.googleapis.com/ajax/libs/jqueryui/1/themes/flick/jquery-ui.css");
+$essentials->includeJS("//ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js");
+$essentials->includeJS("//delfinicdn.appspot.com/javascripts/delfini.all.min.js");
+?>
